d093a979dd445c3ef2f4b390588abfd3bdd2f46d
rogerchen12 <roger.h.chen12@gmail.com>
2021-12-07 20:15:03 -0600

    first map!

---
+++ core/assets/tilesets/clifftiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="clifftiles" tilewidth="32" tileheight="32" tilecount="240" columns="15">
+ <image source="Cliff_tileset.png" width="503" height="518"/>
+</tileset>
---
+++ desktop/src/com/realm/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.realm.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.realm.Realm;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Realm(), config);
+	}
+}
